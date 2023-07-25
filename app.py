5306a7f331707b91eaa2cf204f260e39bf5f7c54
Figu-dev <138683462+Figu-dev@users.noreply.github.com>
2023-07-25 13:25:27 +0000

    Añadimos datos dinámicos con JSON y la ruta API

--- app.py
+++ app.py
@@ -1,11 +1,37 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
+
+## creamos una lista con los trabajos para mostrar de forma dinámica
+JOBS = [
+  {
+    'id': 2,
+    'title': 'Data Scientist',
+    'location': 'Zaragoza, Spain',
+    'salary': '20.000 €'
+  },
+  {
+    'id': 3,
+    'title': 'Frontend Engineer',
+    'location': 'Madrid, Spain'
+  },
+  {
+    'id': 4,
+    'title': 'Backtend Engineer',
+    'location': 'Bilbao, Spain',
+    'salary': '50.000 €'
+  },
+]
 
 
 @app.route("/")
 def hello_world():
-  return "<p>Hello, World!<br>Cómo estás?</p>"
+  return render_template('home.html', jobs=JOBS)
+
+
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
 
 
 if __name__ == '__main__':
